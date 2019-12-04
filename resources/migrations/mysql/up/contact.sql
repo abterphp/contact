566022caea23ae02a8c127a697414ea5d3a48c59
Peter Aba <peter@peteraba.com>
2019-12-04 01:38:58 +0100

    Migration file fixes

--- resources/migrations/mysql/up/contact.sql
+++ resources/migrations/mysql/up/contact.sql
@@ -9,14 +9,13 @@ VALUES (UUID(), 'contactforms');
 -- Table data for table `pages`
 --
 
-INSERT INTO `pages` (`id`, `identifier`, `title`, `meta_description`, `meta_robots`, `meta_author`, `meta_copyright`,
-                     `meta_keywords`, `meta_og_title`, `meta_og_image`, `meta_og_description`, `body`, `header`,
-                     `footer`, `css_files`, `js_files`, `layout_id`, `layout`, `created_at`, `updated_at`, `deleted`)
-VALUES (UUID(), 'contact-success', 'Contact Success', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '',
-        NOW(), NOW(), 0),
-       (UUID(), 'contact-error', 'Contact Error', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '', NOW(),
-        NOW(), 0);
-
+INSERT INTO `pages` (id, identifier, title, meta_description, meta_robots, meta_author, meta_copyright, meta_keywords,
+                     meta_og_title, meta_og_image, meta_og_description, lead, body, category_id, layout_id, layout,
+                     header, footer, css_files, js_files, is_draft)
+VALUES (UUID(), 'contact-success', 'Contact Success', '', '', '', '', '', '', '', '', '', '', NULL, NULL,
+        '', '', '', '', '', 0),
+       (UUID(), 'contact-error', 'Contact Error', '', '', '', '', 'cms, open source', '', '', '', '', '', NULL, NULL,
+        '', '', '', '', '', 0);
 
 --
 -- Table structure and data for table `contact_forms`
